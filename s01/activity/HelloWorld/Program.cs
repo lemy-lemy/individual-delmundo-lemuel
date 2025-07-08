a91ca49f3a329893e0cb5a74c3e52f73fd425122
lemueldelmundo009 <lemueldelmundo009@gmail.com>
2025-07-08 11:34:48 +0800

    Add s01 & s02

---
+++ s01/activity/HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What is your name?");
+Console.Write("");
+
+
+string name = Console.ReadLine();
+
+Console.WriteLine("Hello, " + name + "! Welcome to .NET");
+
+Console.ReadLine();
+
