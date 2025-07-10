c6839a66aed6e047ba2c154af01c148f10ae48a9
lemueldelmundo009 <lemueldelmundo009@gmail.com>
2025-07-10 11:32:52 +0800

    Add activity s04

---
+++ s04/activity/NexusSCM/Nexus.Core/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nexus.Core
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(100)]
+        public string? Description { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(8,2)")]
+        public decimal Price { get; set; }
+
+        public int SupplierId { get; set; }
+        
+        public Supplier Supplier { get; set; }
+    }
+}
