cfaab42ce556645526ff7fec343fbc792afa69fd
lemueldelmundo009 <lemueldelmundo009@gmail.com>
2025-07-11 11:06:40 +0800

    Add activity code s05

---
+++ s05/activity/NexusSCM/Nexus.Web/Controllers/SuppliersController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Nexus.Core;
+using Nexus.Web.Data;
+
+namespace Nexus.Web.Controllers
+{
+    public class SuppliersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SuppliersController (ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var suppliers = await _context.Suppliers.ToListAsync();
+            return View(suppliers);        
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id==null)
+            {
+                return NotFound();
+            }
+            var supplier = await _context.Suppliers.FirstOrDefaultAsync(m=>m.Id == id);
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+            return View(supplier);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Supplier model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            _context.Suppliers.Add(model);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ s05/discussion/NexusSCM/Nexus.API/Controllers/SuppliersController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Nexus.API.Data;
+using Nexus.Core;
+
+namespace Nexus.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuppliersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SuppliersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Supplier>>> GetSuppliers()
+        {
+            return await _context.Suppliers.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Supplier>> GetSupplier(int id)
+        {
+            var supplier = await _context.Suppliers.FindAsync(id);
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+            return Ok(supplier);
+        }
+    }
+}
