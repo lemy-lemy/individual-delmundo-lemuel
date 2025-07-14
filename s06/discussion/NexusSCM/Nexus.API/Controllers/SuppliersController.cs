4942ae0f047a4a5dd697cf5bfb64c9f44fbaf35d
lemueldelmundo009 <lemueldelmundo009@gmail.com>
2025-07-14 11:59:48 +0800

    add activity code s06

---
+++ s06/discussion/NexusSCM/Nexus.API/Controllers/SuppliersController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Nexus.API.Data;
+using Nexus.Core;
+
+namespace Nexus.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuppliersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SuppliersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Supplier>>> GetSuppliers()
+        {
+            //throw new Exception("This is a deliberate test exception!");
+            return await _context.Suppliers.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Supplier>> GetSupplier(int id)
+        {
+            var supplier = await _context.Suppliers.FindAsync(id);
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+            return Ok(supplier);
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<Supplier>>> PostSupplier(Supplier supplier)
+        {
+            if (supplier == null)
+            {
+                return BadRequest();
+            }
+
+            _context.Suppliers.Add(supplier);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(PostSupplier), new { id = supplier.Id }, supplier);
+
+        }
+    }
+}
---
+++ s06/discussion/NexusSCM/Nexus.API/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using Nexus.API.Data;
+using Nexus.API.Data.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseMiddleware<GlobalExceptionHandler>();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+
+app.Run();
